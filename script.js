3122267aac0c72d914a3f81751b91eef902ebf3a
MatheusP0104 <105940986+MatheusP0104@users.noreply.github.com>
2023-01-28 10:04:33 -0300

    V1.0.0

---
+++ script.js
@@ -0,0 +1,35 @@
+// Nav Bar Burguer Menu
+const burguer = document.querySelector(".burguer")
+const navMenu = document.querySelector(".nav-menu")
+
+burguer.addEventListener("click", () => {
+    burguer.classList.toggle('active')
+    navMenu.classList.toggle('active')
+
+})
+// Fim do burguer menu
+
+// Slider Carousel automático
+var slideIndex = 0;
+var slides = document.getElementsByClassName("slide");
+var indicators = document.getElementsByClassName("indicator");
+
+//Função para mostrar o slide atual e esconder os outros
+function showSlides() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    indicators[i].className = indicators[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = "block";
+  indicators[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
+// fim do sleider carousel
+
+  
